a6224caa9cb2c1c98ed0ae72068fd843e30fd219
Oluwaferanmi Adedokun <adedokunoluwaferanmi@gmail.com>
2021-01-02 01:19:28 +0100

    feat: conversion

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,25 +3,22 @@ const label = document.querySelector('.grid__base-label');
 const textField = document.querySelector('.grid__converter__area')
 const baseOption = document.querySelector('#grid__converter__base-selector')
 const otherOption = document.querySelector('#grid__converter__other-selector')
+const loading = document.querySelector('.loading')
 const button = document.querySelector('.button')
+const converterResult = document.querySelector('.converter_value')
 
 document.addEventListener('DOMContentLoaded', () => {
   const data = []
   const output = []
 
-  if (data.length < 0) {
-    let loadingText =  `
-      <h3>Please wait while the data is processing...</h3>
-    `
-    currencyList.innerHTML += loadingText
-  }
   fetch('https://api.frankfurter.app/latest?from=USD')
     .then(response => response.json())
     .then(value => {
       data.push(value)   
       label.innerHTML = data[0].base   
       constructDom(data[0])
-      constructSelectValues(data[0])      
+      constructSelectValues(data[0]) 
+      loading.style.display = 'none';     
     })
 
     const fetchCurrencyName = async () => {
@@ -64,13 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function constructSelectValues(data) {
     baseOption.innerHTML = ''
     otherOption.innerHTML = ''
-    let eachCurrencyName = Object.keys(data.rates).sort((a, b) => {
-      if (a > b) {
-        return a
-      } else {
-        return b
-      }
-    })
+    let eachCurrencyName = Object.keys(data.rates)
     for (let i = 0; i < eachCurrencyName.length; i++) {
       let options = `
         <option value=${eachCurrencyName[i]}>${eachCurrencyName[i]}</option>
@@ -85,27 +76,33 @@ document.addEventListener('DOMContentLoaded', () => {
  const values = {}
   textField.addEventListener('keyup', (e) => {
     let { name, value} = e.target
-    values[name] = value;
-    console.log(values)
+    values[name] = value;    
   })
   baseOption.addEventListener('click', (e) => {
     let { name, value} = e.target
-    values[name] = value;
-    console.log(values)
+    values[name] = value;    
   })  
   otherOption.addEventListener('click', (e) => {
     let { name, value} = e.target
-    values[name] = value;
-    console.log(values)
+    values[name] = value;    
   })
+  function parseValue()
   function convert(base, other, amount) {    
-    /*const host = 'api.frankfurter.app';
+    const host = 'api.frankfurter.app';
     fetch(`https://${host}/latest?amount=${amount}&from=${base}&to=${other}`)
       .then(resp => resp.json())
-      .then((data) => {
-        alert(`10 GBP = ${data.rates.USD} USD`);
-      });*/
-      console.log(`amount conveted is ${amount} from ${base} to ${other}`)
+      .then((data) => { 
+        let value = JSON.stringify(data.rates[other])       
+        let statement = `${amount} ${base} = ${value} ${other}`;
+        converterResult.classList.add('active')
+        converterResult.innerHTML += statement;
+        setTimeout(() => {
+          converterResult.classList.remove('active')
+          converterResult.innerHTML = '';
+        }, 2800)
+        
+        
+      })      
   }
   button.addEventListener('click', (e) => {
     e.preventDefault()
